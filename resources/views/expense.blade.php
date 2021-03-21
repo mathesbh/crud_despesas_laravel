3a746e5b683310bef45ca215ff649ba200ab7998
mathesbh <mathesbh@gmail.com>
2021-03-21 12:30:24 -0300

    Cria view despesas

---
+++ resources/views/expense.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+   <div class="p-5">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+              <h2>Despesas</h2>
+          </div>
+      </div>
+  </div>
+</x-app-layout>
